c537ab782e07b338967eb305e398ba8b611254ef
Andrew Swanson <a.swan89@outlook.com>
2022-10-05 19:55:08 -0500

    added flake

---
+++ flake.nix
@@ -0,0 +1,61 @@
+{
+  description = "A zettelkasten using bash and GNU recutils";
+
+  # Nixpkgs / NixOS version to use.
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+
+      # to work with older version of flakes
+      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+
+      # System types to support.
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlays.default ]; });
+
+    in
+
+    {
+
+      # A Nixpkgs overlay.
+      overlays.default = final: prev: {
+
+        zettelbashten = with final; stdenv.mkDerivation rec {
+          name = "zettelbashten";
+          version = "v0.2";
+          src = fetchFromGitHub {
+            owner = "aswan89";
+            repo = name;
+            rev = version;
+            sha256 = "0m8162zk7iq7f7m93lspkwhykj4hk56dmr99jhb6hjhv3r6z2lhy";
+          };
+          patchPhase = "patchShebangs zettel_create";
+          buildInputs = [ makeWrapper ];
+          installPhase = let deps = lib.makeBinPath [ recutils ]; in 
+            ''
+              mkdir -p $out/bin
+              cp zettel_create $out/bin/zettel_create
+              wrapProgram $out/bin/zettel_create --prefix PATH : "${deps}"
+            '';
+        };
+
+      };
+
+      # Provide some binary packages for selected system types.
+      packages = forAllSystems (system:
+        {
+          inherit (nixpkgsFor.${system}) zettelbashten;
+        });
+
+      # The default package for 'nix build'. This makes sense if the
+      # flake provides only one package or there is a clear "main"
+      # package.
+      defaultPackage = forAllSystems (system: self.packages.${system}.zettelbashten);
+    };
+}
